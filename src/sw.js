96bc61ab91cc48dce86df30948f2ab478aa11368
edipojuan <edipojs@gmail.com>
2019-09-24 01:59:38 -0400

    Implemented cache in service worker

---
+++ src/sw.js
@@ -0,0 +1,19 @@
+const cacheName = 'v1';
+const resourcesToPrecache = [
+  '/',
+  'index.html'
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUtil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll(resourcesToPrecache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(cacheResponse => (cacheResponse || fetch(event.request)))
+  )
+})
